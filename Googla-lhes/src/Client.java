f18ca101fb1ee4f1eb99da183dc50e4a0e9251be
cesar <cesar@DESKTOP-4H8SNGV.lan>
2020-03-21 18:53:44 +0000

    deleted gui as test

--- Googla-lhes/src/Client.java
+++ Googla-lhes/src/Client.java
@@ -98,84 +98,8 @@ public class Client {
 		return keywordMatchingIndexes;
 	}
 	
-	public static void drawGUI (int width, int height) throws IOException {
-		JFrame frame= new JFrame("ISCTE Searcher");
-		frame.setLayout(new BorderLayout()); 
-		
-		//North: Search Panel
-		JPanel searchPanel = new JPanel();
-		FlowLayout searchPanelLayout = new FlowLayout();
-		searchPanel.setLayout(searchPanelLayout);
-		JTextField searchText = new JTextField("", 25);
-		searchPanel.add(searchText);
-		JButton searchButton = new JButton("Search");
-		searchPanel.add(searchButton);
-		
-		
-		//South: Titles and Text Panel
-		JPanel titlesAndTextPanel = new JPanel();
-		BorderLayout titlesAndTextLayout = new BorderLayout();
-		titlesAndTextLayout.setVgap(0);
-		titlesAndTextPanel.setLayout(titlesAndTextLayout);
-		
-		//Titles Panel
-		JPanel titlesPanel = new JPanel();
-		addFilesFromFolderToList(folder);
-		ArrayList<Integer> keywordMatchingIndexes = searchForKeyWordInFiles("Marcelo");
-		for (int index : keywordMatchingIndexes) {
-			filesHeader.add(readHeaderFromFileName(filesNamesList.get(index)));
-		}
-		final JList<String> titlesList = new JList<String>(filesHeader.toArray(new String[filesHeader.size()]));	
-		
-		JScrollPane scroll = new JScrollPane(titlesList);
-		scroll.setPreferredSize(new Dimension(width/2, height));
-		titlesPanel.add(scroll);
-		titlesPanel.setPreferredSize(new Dimension(width/2, height));
-		titlesAndTextPanel.add(titlesPanel, BorderLayout.WEST); 
-		
-		frame.add(searchPanel, BorderLayout.NORTH); 
-		//Text Panel
-		JPanel textPanel = new JPanel();
-		
-		JTextArea textArea = new JTextArea ();
-		
-		titlesList.addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent arg0) {
-                if (!arg0.getValueIsAdjusting()) {
-                  String fileName = filesNamesList.get(titlesList.getSelectedIndex());
-                  
-          		try {
-					textArea.setText(titlesList.getSelectedValue().toString() + "\n" + "\n" + readArticleFromFileName(fileName));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-                }
-            }
-        });
-		
-		textArea.setPreferredSize(new Dimension(width/2 -6, height -6));
-		textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-		JScrollPane textScroll = new JScrollPane(textArea);
-		scroll.setPreferredSize(new Dimension((width/2), height));
-		textPanel.add(textScroll);
-		textPanel.setPreferredSize(new Dimension((width/2), height));
-		titlesAndTextPanel.add(textPanel, BorderLayout.EAST); 
-		
-		
-		frame.add(titlesAndTextPanel, BorderLayout.SOUTH); 
-
-		frame.pack();
-		frame.setLocation(2000, 100);
-		frame.setResizable(true); 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		frame.setVisible(true);  
-	}
-	
 	public static void main(String[] args) throws IOException {
-		drawGUI(1400, 800);
+//		drawGUI(1400, 800);
 	}
 	
 }
